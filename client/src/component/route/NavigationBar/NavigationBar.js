34af74b16625ead2847a7400af86614f1fbef98e
Benjamin Qian <qianjingning0903@gmail.com>
2024-07-11 17:58:16 +1000

    add font style and font interaction

--- client/src/component/route/NavigationBar/NavigationBar.js
+++ client/src/component/route/NavigationBar/NavigationBar.js
@@ -7,12 +7,12 @@ const NavigationBar = () => {
       <div className="nav-container">
         <span className="container-el">logo</span>
         <div className="nav-details">
-          <span>_home</span>
-          <span>_about</span>
-          <span>_portoflio</span>
-          <span>_services</span>
-          <span>_resume</span>
-          <span>_contact</span>
+          <span className="text-element">_home</span>
+          <span className="text-element">_about</span>
+          <span className="text-element">_portoflio</span>
+          <span className="text-element">_services</span>
+          <span className="text-element">_resume</span>
+          <span className="text-element">_contact</span>
         </div>
       </div>
 
