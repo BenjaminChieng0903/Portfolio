d5ccacfdef0c1c63dc7a581e7a9c18479b115dc4
Benjamin Qian <qianjingning0903@gmail.com>
2024-07-12 00:30:35 +1000

    dynamic active index

--- client/src/component/route/NavigationBar/NavigationBar.js
+++ client/src/component/route/NavigationBar/NavigationBar.js
@@ -1,18 +1,38 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import "./NavigationBar.css";
+import { useState } from "react";
 
 const NavigationBar = () => {
+  const navigate = useNavigate();
+  const navDetailsArray = [
+    "_home",
+    "_about",
+    "_portoflio",
+    "_services",
+    "_resume",
+    "_contact",
+  ];
+  const [activeIndex, setActiveIndex] = useState(0);
   return (
     <div>
       <div className="nav-container">
         <span className="container-el">logo</span>
         <div className="nav-details">
-          <span className="text-element">_home</span>
-          <span className="text-element">_about</span>
-          <span className="text-element">_portoflio</span>
-          <span className="text-element">_services</span>
-          <span className="text-element">_resume</span>
-          <span className="text-element">_contact</span>
+          {navDetailsArray.map((item, index) => {
+            return (
+              <span
+                key={index}
+                className={`text-element${
+                  activeIndex === index ? "active" : ""
+                }`}
+                onClick={() => {
+                  setActiveIndex(index);
+                }}
+              >
+                {item}
+              </span>
+            );
+          })}
         </div>
       </div>
 
