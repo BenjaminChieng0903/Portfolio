e27b3fd9170146dddd236d1bf148b4ab85cd5ec7
Benjamin Qian <qianjingning0903@gmail.com>
2024-08-01 00:02:52 +1000

    service-page-finished

--- client/src/component/route/ServicesPage/ServicesPage.jsx
+++ client/src/component/route/ServicesPage/ServicesPage.jsx
@@ -1,5 +1,54 @@
+import COntactMe from "../../ContactMe/ContactMe";
+import TypingEffectRewindSlow from "../../TypingEffect/TypingEffectRewindSlow";
+import "./ServicesPage.css";
 const ServicesPage = () => {
-  return <>I'm services page</>;
+  const texts = ["Services"];
+  return (
+    <div className="services-container">
+      <header className="child-hierarchy App-header-services">
+        <TypingEffectRewindSlow texts={texts} speed={150} />
+        <span className="services-section-intro">
+          …lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+          elementum, eros at sagittis facilisis, ante est rutrum ligula, at
+          interdum eros sapien vel magna.
+        </span>
+      </header>
+      <div className="background-image-container"></div>
+      <div className="services-page-body-container">
+        <section className="services-info-container">
+          <span className="grid-border">hi, grid 1</span>
+          <span className="grid-border grid-info-style">
+            …lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ut
+            dui tortor. Maecenas cursus velit sit amet orci tincidunt hendrerit.
+            Etiam tincidunt nisl at nibh ullamcorper, at lacinia arcu egestas.
+            Maecenas sed volutpat enim, ut vulputate sapien;
+          </span>
+          <span className="grid-border">hi, grid 1</span>
+          <span className="grid-border grid-info-style">
+            …lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ut
+            dui tortor. Maecenas cursus velit sit amet orci tincidunt hendrerit.
+            Etiam tincidunt nisl at nibh ullamcorper, at lacinia arcu egestas.
+            Maecenas sed volutpat enim, ut vulputate sapien;
+          </span>
+          <span className="grid-border">hi, grid 1</span>
+          <span className="grid-border grid-info-style">
+            …lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ut
+            dui tortor. Maecenas cursus velit sit amet orci tincidunt hendrerit.
+            Etiam tincidunt nisl at nibh ullamcorper, at lacinia arcu egestas.
+            Maecenas sed volutpat enim, ut vulputate sapien;
+          </span>
+          <span className="grid-border">hi, grid 1</span>
+          <span className="grid-border grid-info-style">
+            …lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ut
+            dui tortor. Maecenas cursus velit sit amet orci tincidunt hendrerit.
+            Etiam tincidunt nisl at nibh ullamcorper, at lacinia arcu egestas.
+            Maecenas sed volutpat enim, ut vulputate sapien;
+          </span>
+        </section>
+        <COntactMe></COntactMe>
+      </div>
+    </div>
+  );
 };
 
 export default ServicesPage;
