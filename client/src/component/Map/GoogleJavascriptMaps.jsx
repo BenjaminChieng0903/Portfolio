41739aaaaf7d7227d63eb0f7053252ab75d217c0
Benjamin Qian <qianjingning0903@gmail.com>
2024-08-01 21:19:49 +1000

    google-js-map-api setup

---
+++ client/src/component/Map/GoogleJavascriptMaps.jsx
@@ -0,0 +1,31 @@
+import { APIProvider, Map, Marker } from "@vis.gl/react-google-maps";
+const GoogleJavascriptMaps = () => {
+  const API_KEY =
+    globalThis.GOOGLE_MAPS_API_KEY ?? process.env.GOOGLE_MAPS_API_KEY;
+  const position = { lat: -37.81521987915039, lng: 144.9383544921875 };
+  return (
+    <APIProvider
+      apiKey={API_KEY}
+      onLoad={() => console.log("Maps API has loaded.")}
+    >
+      <Map
+        className="map"
+        mapId="DEMO_MAP_ID"
+        defaultZoom={13}
+        defaultCenter={position}
+        onCameraChanged={(ev) =>
+          console.log(
+            "camera changed:",
+            ev.detail.center,
+            "zoom:",
+            ev.detail.zoom
+          )
+        }
+      >
+        <Marker position={position}></Marker>
+      </Map>
+    </APIProvider>
+  );
+};
+
+export default GoogleJavascriptMaps;
