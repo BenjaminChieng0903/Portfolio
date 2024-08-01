f828cf1909a2522c54eac4d1b8d6458f22580ba1
Benjamin Qian <qianjingning0903@gmail.com>
2024-08-01 23:55:52 +1000

    finalize-contact-me-page

--- client/src/component/route/ContactPage/ContactPage.jsx
+++ client/src/component/route/ContactPage/ContactPage.jsx
@@ -11,13 +11,15 @@ const ContactPage = () => {
         <div className="address-grid">
           <div className="phone-details">
             <span>&#123;phone&#125;</span>
-            <span>123456</span>
+            <span>434-589-236</span>
           </div>
           <div className="email-details">
             <span>&#123;email&#125;</span>
+            <span>qianjingning0903@gmail.com</span>
           </div>
           <div className="address-details">
             <span>&#123;address&#125;</span>
+            <span>8 Pearl River Rd, Docklands, VIC 3008</span>
           </div>
         </div>
         <GoogleJavascriptMaps></GoogleJavascriptMaps>
