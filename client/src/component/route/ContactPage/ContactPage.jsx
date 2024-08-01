40578bc45dcec5b986cddcb33f32bd4a6259b49c
Benjamin Qian <qianjingning0903@gmail.com>
2024-08-01 21:20:31 +1000

    contact-me-section import

--- client/src/component/route/ContactPage/ContactPage.jsx
+++ client/src/component/route/ContactPage/ContactPage.jsx
@@ -1,5 +1,29 @@
+import COntactMe from "../../ContactMe/ContactMe";
+import GoogleJavascriptMaps from "../../Map/GoogleJavascriptMaps";
+import "./ContactPage.css";
+
 const ContactPage = () => {
-  return <>I'm contact page</>;
+  return (
+    <div className="contact-me-container">
+      <div className="background-image-container"></div>
+      <div className="contact-page-body-container">
+        <COntactMe></COntactMe>
+        <div className="address-grid">
+          <div className="phone-details">
+            <span>&#123;phone&#125;</span>
+            <span>123456</span>
+          </div>
+          <div className="email-details">
+            <span>&#123;email&#125;</span>
+          </div>
+          <div className="address-details">
+            <span>&#123;address&#125;</span>
+          </div>
+        </div>
+        <GoogleJavascriptMaps></GoogleJavascriptMaps>
+      </div>
+    </div>
+  );
 };
 
 export default ContactPage;
