31955536474cd1a69f1fd92bdc692da267ef977a
Benjamin Qian <qianjingning0903@gmail.com>
2024-08-02 15:10:12 +1000

    portfolio-page-header

--- client/src/component/route/Portoflio/Portoflio.jsx
+++ client/src/component/route/Portoflio/Portoflio.jsx
@@ -1,5 +1,38 @@
+import COntactMe from "../../ContactMe/ContactMe";
+import TypingEffectRewindSlow from "../../TypingEffect/TypingEffectRewindSlow";
+import "./Portoflio.css";
 const Portoflio = () => {
-  return <>I'm portoflio</>;
+  const texts = ["Portfolio"];
+  const filterTitle = [
+    "All Projects",
+    "Web Development",
+    "Web Application",
+    "System"
+  ];
+  return (
+    <div className="portfolio-container">
+      <header className="child-hierarchy App-header-portfolio">
+        <TypingEffectRewindSlow texts={texts} speed={150} />
+        <span className="portfolio-section-intro">
+          …lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+          elementum, eros at sagittis facilisis, ante est rutrum ligula, at
+          interdum eros sapien vel magna.
+        </span>
+      </header>
+      <div className="portfolio-page-body-container">
+        <div className="portfolio-filter">
+          <div className="filter">
+            <button>{filterTitle[0]}</button>
+            <button>{filterTitle[1]}</button>
+            <button>{filterTitle[2]}</button>
+            <button>{filterTitle[3]}</button>
+          </div>
+          <div className="project"></div>
+        </div>
+        <COntactMe></COntactMe>
+      </div>
+    </div>
+  );
 };
 
 export default Portoflio;
