3b1c5dfe9732f21ca623d1ef531bd51782854cf6
Benjamin Qian <qianjingning0903@gmail.com>
2024-08-26 02:06:44 +1000

    sending-email-async-execution
    sending-email-async-execution

--- server/src/controller/homeController.js
+++ server/src/controller/homeController.js
@@ -6,12 +6,12 @@ const homeActions = {
  submitEnquiry: async(req, res, next)=>{
   //get user information from front
     console.log(req.body);
-    if(req.body){
-    (await sendEmail(req.body));
-    }
     res.status(200).json({
         status:"success"
     })
+    if(req.body){
+      (await sendEmail(req.body));
+      }
   //pass user information as parameters
 
   //send email
